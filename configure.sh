a4fd3084e75e4bea6dd51ea6e8285608708311e9
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2023-01-06 22:30:24 +0530

    Added configuration script

--- configure.sh
+++ configure.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
+LOCATION=$HOME
 
+echo "Backing up .bashrc, .bash_aliases and .inputrc"
+
+if [ -f $HOME/.bashrc ]; then
+  mv $HOME/.bashrc $HOME/.bashrc.bak
+fi
+
+if [ -f $HOME/.bash_aliases ]; then
+  mv $HOME/.bash_aliases $HOME/.bash_aliases.bak
+fi
+
+if [ -f $HOME/.inputrc ]; then
+  mv $HOME/.inputrc $HOME/.inputrc.bak
+fi
+
+echo "Downloading new config..."
+
+curl https://raw.githubusercontent.com/AkashKarnatak/digitalocean-setup/main/.bashrc -o ~/.bashrc
+curl https://raw.githubusercontent.com/AkashKarnatak/digitalocean-setup/main/.bash_aliases -o ~/.bash_aliases
+curl https://raw.githubusercontent.com/AkashKarnatak/digitalocean-setup/main/.inputrc -o ~/.inputrc
+
+echo -e "\nLoad new configuration using,\n\tsource ~/.bashrc"
