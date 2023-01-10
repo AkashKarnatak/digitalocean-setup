637131315f872fc9a8ef3581ac01d60efc509d7a
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2023-01-10 20:07:50 +0530

    Added configuration for tmux.conf

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 LOCATION=$HOME
 
-echo -e "\n\nBacking up .bashrc, .bash_aliases and .inputrc"
+echo -e "\n\nBacking up .bashrc, .bash_aliases, .inputrc and .tmux.conf"
 
 if [ -f $HOME/.bashrc ]; then
   mv $HOME/.bashrc $HOME/.bashrc.bak
@@ -16,10 +16,15 @@ if [ -f $HOME/.inputrc ]; then
   mv $HOME/.inputrc $HOME/.inputrc.bak
 fi
 
+if [ -f $HOME/.tmux.conf ]; then
+  mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
+fi
+
 echo -e "\nDownloading new config..."
 
 curl https://raw.githubusercontent.com/AkashKarnatak/digitalocean-setup/main/.bashrc -o ~/.bashrc
 curl https://raw.githubusercontent.com/AkashKarnatak/digitalocean-setup/main/.bash_aliases -o ~/.bash_aliases
 curl https://raw.githubusercontent.com/AkashKarnatak/digitalocean-setup/main/.inputrc -o ~/.inputrc
+curl https://raw.githubusercontent.com/AkashKarnatak/digitalocean-setup/main/.tmux.conf -o ~/.tmux.conf
 
 echo -e "\n\nLoad new configuration using,\n\tsource ~/.bashrc && bind -f  ~/.inputrc"
